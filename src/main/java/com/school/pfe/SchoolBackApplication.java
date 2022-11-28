f56ef6dad266629d906843d26aa339860ffb3331
Your Name <you@example.com>
2022-11-29 00:45:01 +0100

    squelette

---
+++ src/main/java/com/school/pfe/SchoolBackApplication.java
@@ -0,0 +1,13 @@
+package com.school.pfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/school/pfe/SchoolBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.pfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
