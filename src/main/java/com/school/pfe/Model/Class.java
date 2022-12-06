2713aa8f92d23d1b49cc78e27b20cf4012e57d7b
jemai-sameh <samehjemai98@gmail.com>
2022-12-06 14:49:54 +0100

    create service and controller for entity class

--- src/main/java/com/school/pfe/Model/Class.java
+++ src/main/java/com/school/pfe/Model/Class.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@SuperBuilder
 @Getter
 @Setter
 @Entity
